e9910892d18754d0b26ad3db2e5b269bed10afe4
kukiss <wwtt1984@163.com>
2015-03-26 15:25:42 +0800

    2015-03-06 wy

--- app/store/ProjectMenuStore.js
+++ app/store/ProjectMenuStore.js
@@ -11,9 +11,9 @@ Ext.define('YzMobile.store.ProjectMenuStore', {
         model: 'YzMobile.model.ProjectMenuModel',
 
         data: [
-            {id: '01', name: 'rain', title: '水库', url: 'resources/icons/水库.png'},
-            {id: '02', name: 'rain', title: '水闸', url: 'resources/icons/闸门.png'},
-            {id: '03', name: 'rain', title: '提防', url: 'resources/icons/闸门.png'},
+            {id: '01', name: 'rain', title: '水库', url: 'resources/icons/reservoir.png'},
+            {id: '02', name: 'rain', title: '水闸', url: 'resources/icons/gate.png'},
+            {id: '03', name: 'rain', title: '提防', url: 'resources/icons/gate.png'},
             {id: '04', name: 'rain', title: '堰坝', url: 'resources/images/function/rain.png'},
             {id: '05', name: 'rain', title: '水电站', url: 'resources/images/function/rain.png'},
             {id: '06', name: 'norain', title: '山塘', url: 'resources/images/function/rain.png'}
